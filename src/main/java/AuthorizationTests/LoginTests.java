f90c52b64c9554b8cd59ca8f9fa2adcebbe00af9
Ivan <Lihoy92ivan@gmail.com>
2017-03-09 16:45:54 +0200

    version 1.2

--- src/main/java/AuthorizationTests/LoginTests.java
+++ src/main/java/AuthorizationTests/LoginTests.java
@@ -1,9 +1,11 @@
 package AuthorizationTests;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
@@ -33,66 +35,66 @@ public class LoginTests {
         }
         @AfterMethod(alwaysRun = true)
         public static void teardown(){
-            driver.quit();
+            //driver.quit();
         }
 
         //@Parameters({"loginEmail", "loginPassword"})
-        @Test()
-        public static void PositiveLoginTest(String email, String Password) {
-            WebElement signin_button = driver.findElement(By.xpath(".//*[@id='myCarousel']/div[2]/div/nav/div/div[2]/ul/li[3]/a"));
-            signin_button.click();
-            sleep(3);
-            WebElement login_field = driver.findElement(By.id("exampleInputEmail1"));
-            login_field.sendKeys("v.lihoy+1@mail.ru");
-            WebElement password_field = driver.findElement(By.id("exampleInputPassword1"));
-            password_field.sendKeys("111111");
-            WebElement sign_in_button = driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[2]/div/div/form[2]/button"));
-            sign_in_button.click();
-            sleep(3);
-            Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='content']/div[2]/div[1]/div/div[1]")).isDisplayed());
-            Assert.assertEquals("Carvoy", driver.getTitle());
-            System.out.println("User is logged in");
-
-            driver.manage().timeouts().implicitlyWait(0, TimeUnit.MILLISECONDS);
-            boolean exists = driver.findElements( By.id(".//*[@id='content']/div[2]/div[1]/div/div[1]") ).size() != 0;
-            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-
-        }
-        @Test(dataProvider = "data-provider")
-        public static void NegativeLoginTests(String email, String pass){
-            WebElement signin_button = driver.findElement(By.xpath(".//*[@id='myCarousel']/div[2]/div/nav/div/div[2]/ul/li[3]/a"));
-            signin_button.click();
-            sleep(3);
-            WebElement login_field = driver.findElement(By.id("exampleInputEmail1"));
-            login_field.sendKeys(email);
-            WebElement password_field = driver.findElement(By.id("exampleInputPassword1"));
-            password_field.sendKeys(pass);
-            WebElement sign_in_button = driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[2]/div/div/form[2]/button"));
-            sign_in_button.click();
-            sleep(3);
-            Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[1]/button")).isDisplayed());
-
-            boolean present;
-            try {
-                driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[1]/button"));
-                present = true;
-            } catch (NoSuchElementException e) {
-                present = false;
-            }
-        }
-        @DataProvider(name = "data-provider")
-        public Object[][] dataProviderMethod(){
-            Object[][] data = new Object[4][2];
-            data[0][0] = "v.lihoy+2mail.ru";
-            data[0][1] = "111111";
-            data[1][0] = "v.lihoy+1@mail.ru";
-            data[1][1] = "";
-            data[2][0] = "";
-            data[2][1] = "111111";
-            data[3][0] = "";
-            data[3][1] = "";
-            return data;
-        }
+//        @Test()
+//        public static void PositiveLoginTest(String email, String Password) {
+//            WebElement signin_button = driver.findElement(By.xpath(".//*[@id='myCarousel']/div[2]/div/nav/div/div[2]/ul/li[3]/a"));
+//            signin_button.click();
+//            sleep(3);
+//            WebElement login_field = driver.findElement(By.id("exampleInputEmail1"));
+//            login_field.sendKeys("v.lihoy+1@mail.ru");
+//            WebElement password_field = driver.findElement(By.id("exampleInputPassword1"));
+//            password_field.sendKeys("111111");
+//            WebElement sign_in_button = driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[2]/div/div/form[2]/button"));
+//            sign_in_button.click();
+//            sleep(3);
+//            Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='content']/div[2]/div[1]/div/div[1]")).isDisplayed());
+//            Assert.assertEquals("Carvoy", driver.getTitle());
+//            System.out.println("User is logged in");
+//
+//            driver.manage().timeouts().implicitlyWait(0, TimeUnit.MILLISECONDS);
+//            boolean exists = driver.findElements( By.id(".//*[@id='content']/div[2]/div[1]/div/div[1]") ).size() != 0;
+//            driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+//
+//        }
+//        @Test(dataProvider = "data-provider")
+//        public static void NegativeLoginTests(String email, String pass){
+//            WebElement signin_button = driver.findElement(By.xpath(".//*[@id='myCarousel']/div[2]/div/nav/div/div[2]/ul/li[3]/a"));
+//            signin_button.click();
+//            sleep(3);
+//            WebElement login_field = driver.findElement(By.id("exampleInputEmail1"));
+//            login_field.sendKeys(email);
+//            WebElement password_field = driver.findElement(By.id("exampleInputPassword1"));
+//            password_field.sendKeys(pass);
+//            WebElement sign_in_button = driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[2]/div/div/form[2]/button"));
+//            sign_in_button.click();
+//            sleep(3);
+//            Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[1]/button")).isDisplayed());
+//
+//            boolean present;
+//            try {
+//                driver.findElement(By.xpath(".//*[@id='wrapper']/div[1]/div/div/div[1]/button"));
+//                present = true;
+//            } catch (NoSuchElementException e) {
+//                present = false;
+//            }
+//        }
+//        @DataProvider(name = "data-provider")
+//        public Object[][] dataProviderMethod(){
+//            Object[][] data = new Object[4][2];
+//            data[0][0] = "v.lihoy+2mail.ru";
+//            data[0][1] = "111111";
+//            data[1][0] = "v.lihoy+1@mail.ru";
+//            data[1][1] = "";
+//            data[2][0] = "";
+//            data[2][1] = "111111";
+//            data[3][0] = "";
+//            data[3][1] = "";
+//            return data;
+//        }
         @Test
         public static void ForgotPasswordLinkPositiveTest(){
             WebElement signin_button = driver.findElement(By.xpath(".//*[@id='myCarousel']/div[2]/div/nav/div/div[2]/ul/li[3]/a"));
@@ -104,7 +106,7 @@ public class LoginTests {
             WebElement email_field = driver.findElement(By.id("exampleInputEmail11"));
             email_field.sendKeys("lihoy@clever-solution.com");
             WebElement submit_button = driver.findElement(By.xpath("html/body/div[1]/div/div/div[2]/div/form/button"));
-            //submit_button.click();
+            submit_button.click();
             Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='myCarousel']/div[2]/div/nav/div/div[2]/ul/li[3]/a")).isDisplayed());
         }
         @Test(dependsOnMethods={"ForgotPasswordLinkPositiveTest"})
@@ -118,14 +120,30 @@ public class LoginTests {
             sleep(3);
             driver.findElement(By.xpath(".//*[@id='nb-1']/body/div[2]/div[4]/div/div[2]/div[3]/div[4]/div[1]/div/div/div[2]/div/div[1]")).click();
             sleep(2);
-            driver.findElement(By.xpath(".//*[@id='nb-1']/body/div[2]/div[4]/div/div[2]/div[3]/div[4]/div[1]/div/div[3]/div/div/p[3]/a")).click();
-            sleep(10);
 
-            WebElement pass = driver.findElement(By.xpath("//*/div/input[@id='recoveryconfirmationform-password']"));
+                              // Open link in new window
+//            driver.findElement(By.xpath(".//*[@id='nb-1']/body/div[2]/div[4]/div/div[2]/div[3]/div[4]/div[1]/div/div[3]/div/div/p[3]/a")).click();
+//            sleep(10);
+//            WebElement link = driver.findElement(By.xpath(".//*[@id='nb-1']/body/div[2]/div[4]/div/div[2]/div[3]/div[4]/div[1]/div/div[3]/div/div/p[3]/a"));
+//            Actions new_win = new Actions(driver);
+//            new_win.keyDown(Keys.SHIFT).click(link).keyUp(Keys.SHIFT).build().perform();
+//            sleep(6);
+                            // Find element in new window
+            driver.findElement(By.xpath(".//*[@id='nb-1']/body/div[2]/div[4]/div/div[2]/div[3]/div[4]/div[1]/div/div[3]/div/div/p[3]/a")).click();
+            String parentHandle = driver.getWindowHandle();
+            for (String winHandle : driver.getWindowHandles()) {
+                driver.switchTo().window(winHandle);
+            }
+
+            WebElement pass = driver.findElement(By.xpath(".//*[@id='recoveryconfirmationform-password']"));
             pass.sendKeys("111111");
-            WebElement re_pass = driver.findElement(By.xpath("//*/div/input[@id='recoveryconfirmationform-repassword']"));
+            WebElement re_pass = driver.findElement(By.xpath(".//*[@id='recoveryconfirmationform-repassword']"));
             re_pass.sendKeys("111111");
-            driver.findElement(By.linkText("Save")).click();
+            driver.findElement(By.xpath(".//*[@id='w0']/fieldset/div[4]/button")).click();
+            sleep(5);
+            Assert.assertTrue(driver.findElement(By.xpath(".//*[@id='myCarousel']/div[2]/div/nav/div/div[2]/ul/li[3]/a/span[1]")).isDisplayed());
+//            driver.close();
+//            driver.switchTo().window(parentHandle);
         }
 }
 
